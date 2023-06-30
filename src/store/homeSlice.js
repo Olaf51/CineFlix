b7360c21610f5c3d746e677013575da551024449
Gagan <gagan512003@gmail.com>
2023-06-30 17:07:59 +0530

    final commit

--- src/store/homeSlice.js
+++ src/store/homeSlice.js
@@ -1,31 +1,22 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
-
-
-export const counterSlice = createSlice({
-  name: 'home',
-  initialState: {
-    url:{},
-    genres:{}
-  },
-  reducers: {
-    increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1
+export const homeSlice = createSlice({
+    name: "home",
+    initialState: {
+        url: {},
+        genres: {},
     },
-    decrement: (state) => {
-      state.value -= 1
+    reducers: {
+        getApiConfiguration: (state, action) => {
+            state.url = action.payload;
+        },
+        getGenres: (state, action) => {
+            state.genres = action.payload;
+        },
     },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload
-    },
-  },
-})
+});
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export const { getApiConfiguration, getGenres } = homeSlice.actions;
 
-export default counterSlice.reducer
+export default homeSlice.reducer;
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,22 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
 
-const BASE_URL = 'https://api.themoviedb.org/3';
-
-const TMDB_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNmNhNjFlZTI0NmRlYjE4Mzg5ZGRhYWZhZmY0YzY1YSIsInN1YiI6IjY0OTcyNTg0NjJmMzM1MDE0NjI4N2VlYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zKH2zGpYYtOnZJH-h0B4C5Be54Yw6oY793Irj7FnV4s";
+const BASE_URL = "https://api.themoviedb.org/3";
+const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
 
 const headers = {
-    Authorization: `Bearer ${TMDB_TOKEN}`,
-}
+    Authorization: "bearer " + TMDB_TOKEN,
+};
 
 export const fetchDataFromApi = async (url, params) => {
-    try{
-       const {data} = await axios.get(`${BASE_URL}${url}`, {
-         headers,
-         params,
-       });
-       return data;
-    } catch(err){
+    try {
+        const { data } = await axios.get(BASE_URL + url, {
+            headers,
+            params,
+        });
+        return data;
+    } catch (err) {
         console.log(err);
         return err;
     }
-}
+};
